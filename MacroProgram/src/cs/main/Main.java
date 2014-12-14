5a7ac2bd32113409e0f93889fb735cc4f84613cd
HKJ <valunas713@gmail.com>
2014-12-14 21:48:49 +0900

    Refactoring Source

--- MacroProgram/src/cs/main/Main.java
+++ MacroProgram/src/cs/main/Main.java
@@ -9,22 +9,18 @@ public class Main {
 	@SuppressWarnings("static-access")
 	public static void main(String[] args) throws Exception {
 
-		
-		
-		CreateExcel ce = new CreateExcel();
-		ce.createExcelFile("", "D://dbinfo.xlsx");
-		
-		CreatePDF cf = new CreatePDF();
-		cf.createPDFFile("", "D://dbinfo.pdf");
-		
-		CreateWord cw = new CreateWord();
-		cw.createWordFile("","D://dbinfo.docx");
-		
-		
-		
-		
-		
-		
-		
+		// CreateExcel ce = new CreateExcel();
+		// ce.createExcelFile("", "D://dbinfo.xlsx");
+		//
+		// CreatePDF cf = new CreatePDF();
+		// cf.createPDFFile("", "D://dbinfo.pdf");
+		//
+		// CreateWord cw = new CreateWord();
+		// cw.createWordFile("","D://dbinfo.docx");
+		//
+		GUI gui = new GUI();
+		gui.show();
+		gui.setSize(320, 180);
+
 	}
 }
